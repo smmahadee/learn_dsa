6eb6fb4c07028fcbe3c8a98412ac7ea6b28db93d
smmahadee <mahadee.codes@gmail.com>
2023-12-01 06:57:30 +0600

    module 10 processing

--- 10.sparse_matrix/sparse_matrix.cpp
+++ 10.sparse_matrix/sparse_matrix.cpp
@@ -21,14 +21,12 @@ class Sparse_Matrix {
         m = 2;
         n = 2;
         num = 1;
-        el = new Element[num];
     }
 
     Sparse_Matrix(int m, int n, int num) {
         this->m = m;
         this->n = n;
         this->num = num;
-        el = new Element[num];
     }
 
     void create() {
@@ -39,9 +37,26 @@ class Sparse_Matrix {
             cin >> el[i].i >> el[i].j >> el[i].x;
         }
     }
+
+    void display() {
+        int i, j, k = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == el[k].i && j == el[k].j) {
+                    cout << el[k++].x << " ";
+                } else {
+                    cout << 0 << " ";
+                }
+            }
+
+            cout << endl;
+        }
+    }
 };
 
-int main() { 
+int main() {
     Sparse_Matrix s;
     s.create();
- }
+    s.display();
+}
--- 7.array_ADT/main.cpp
+++ 7.array_ADT/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
